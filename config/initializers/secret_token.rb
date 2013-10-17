4e9968e4485926f430e97f6121750e80e83e826b
Mark Horowitz <msh345+lists@gmail.com>
2013-10-17 16:25:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PerfectDay::Application.config.secret_key_base = 'c8b32432e471a57a304bb940acd17669897fa266069668a98ae73d997f34c39bb7cf97a81626c62561b181e2c758f819861fe39942affe3cb613b1a8af75b209'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PerfectDay::Application.config.session_store :cookie_store, key: '_perfect_day_session'
