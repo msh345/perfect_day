6c0b3bd5e614d1788823b8d6b47222a02db858cc
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 13:46:38 -0500

    logout functionality and updated user#show view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,11 @@ class SessionsController < ApplicationController
       render :index
     end
   end
+
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
 end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
   end
 
-
   def create
     @user = User.new(user_params)
     if @user.save
