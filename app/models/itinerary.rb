b1370f2d035349d21eab04047736426bc0aac64f
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 13:51:41 -0500

    added Itinerary model/migration and associations

---
+++ app/models/itinerary.rb
@@ -0,0 +1,3 @@
+class Itinerary < ActiveRecord::Base
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :itineraries
   has_secure_password
 
   validates_presence_of :username, :email
