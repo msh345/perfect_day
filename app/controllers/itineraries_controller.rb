04e0bc81051806ca8762ee6dda588c4615b8b8c7
Jake Sikora <jakesikora.01@gmail.com>
2013-10-19 12:38:18 -0500

    edited controller to prevent 400 response

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -11,14 +11,14 @@ class ItinerariesController < ApplicationController
     puts params.inspect
     puts "********************"
     puts "********************"
-    user_itinerary = Itinerary.create(itinerary_params)
-    user_itineraries = current_user.itineraries << user_itinerary
-    params[:itineraries_name].each do |spot_params|
-      spot = Spot.find_or_create_by(address: spot_params[:address])
-      user_itinerary.spots << spot
-      itinerary_spot = spot.itinerary_spots.find_by_itinerary_id(user_itinerary.id)
-      itinerary_spot.update_attributes(description: spot_params[:description])
-    end
+    # user_itinerary = Itinerary.create(itinerary_params)
+    # user_itineraries = current_user.itineraries << user_itinerary
+    # params[:itineraries_name].each do |spot_params|
+    #   spot = Spot.find_or_create_by(address: spot_params[:address])
+    #   user_itinerary.spots << spot
+    #   itinerary_spot = spot.itinerary_spots.find_by_itinerary_id(user_itinerary.id)
+    #   itinerary_spot.update_attributes(description: spot_params[:description])
+    # end
 
     redirect_to user_path(current_user)
   end
