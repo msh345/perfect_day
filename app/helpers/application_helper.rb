3c72b9fbd771477486dd9b64e9cad41e164f96d2
Jake Sikora <jakesikora.01@gmail.com>
2013-10-23 14:19:19 -0500

    maths for distance calculations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+    def calculate_distance(your_latlong, destination_latlong) # as the crow flies
+    radius = 6371
+    d_lat  = to_radian(destination_latlong[0] - your_latlong[0])
+    d_lon  = to_radian(destination_latlong[1] - your_latlong[1])
+    lat_1  = to_radian(your_latlong[0])
+    lat_2  = to_radian(destination_latlong[0])
+
+    a = Math.sin(d_lat/2) * Math.sin(d_lat/2) +
+        Math.sin(d_lon/2) * Math.sin(d_lon/2) * Math.cos(your_latlong[0]) * Math.cos(destination_latlong[0])
+
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+
+    distance = (radius * c) * 0.6214
+  end
+
+  def to_radian(number)
+    (number * Math::PI) / 180
+  end
 end
